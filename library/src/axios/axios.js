9c920489bd8d659b8dbaf056be8070c2a01ec719
harish-nagubadi <Harish.N@veots.com>
2025-03-18 16:13:00 +0530

    cors upadte

--- library/src/axios/axios.js
+++ library/src/axios/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const BASE_URL = "http://localhost:3009" 
+const BASE_URL = "https://library-management-5d01.onrender.com" 
 
 const axiosInstance=axios.create({
     baseURL:BASE_URL,

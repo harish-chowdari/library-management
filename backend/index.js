cd283d1cb759f6eaac79ed7ae3fe75da7c085296
harish-nagubadi <Harish.N@veots.com>
2025-03-18 16:09:47 +0530

    cors upadte

--- backend/index.js
+++ backend/index.js
@@ -15,23 +15,24 @@ const allowedDomains=[
     "http://localhost:3002",
     "http://localhost:3003", 
     "http://localhost:3004", 
-    "http://localhost:3005" 
+    "http://localhost:3005",
+    "https://library-management-alpha-swart.vercel.app" 
 ]
 
-app.use(cors());
-// app.use(cors({  
-//     credentials:true,
-//     origin:function (origin, callback) {
-//         // bypass the requests with no origin
-//         if (!origin) return callback(null, true);
-//         if (allowedDomains.indexOf(origin) === -1) {
-//             let msg = `This site ${origin} does not have an access. Only specific domains are allowed to access it.`;
-//             return callback(new Error(msg), false);
-//         }
-//         return callback(null, true);
-//     },
-//     exposedHeaders:['Content-Disposition']
-// }));
+// app.use(cors());
+app.use(cors({  
+    credentials:true,
+    origin:function (origin, callback) {
+        // bypass the requests with no origin
+        if (!origin) return callback(null, true);
+        if (allowedDomains.indexOf(origin) === -1) {
+            let msg = `This site ${origin} does not have an access. Only specific domains are allowed to access it.`;
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    },
+    exposedHeaders:['Content-Disposition']
+}));
 
 const librarianRoutes = require("./routes/librarianRoutes");
 const authRoutes = require("./routes/authRoutes");
